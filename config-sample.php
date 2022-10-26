e7b0623733866c10570c2ac06fa9ff8d87c2e998
sgilyin <developer@ilyins.ru>
2022-10-26 10:13:35 +0500

    Оповещения в ВК и ТГ

--- config-sample.php
+++ config-sample.php
@@ -1,5 +1,23 @@
 <?php
 
+# BGBilling Config
 define('BGB_HOST', '<Enter here>');
 define('BGB_USER', '<Enter here>');
 define('BGB_PASSWORD', '<Enter here>');
+
+# Bitrix24 Config
+define('CRM_HOST', '<Enter here>');
+define('CRM_SECRET', '<Enter here>');
+
+# Telegram Bot Config
+define('TELEGRAM_BOT_TOKEN', '<Enter here>');
+
+# VK API Config
+define('VK_API_TOKEN', '<Enter here>');
+define('VK_API_VERSION','5.131');
+
+# DB Config
+define('DB_HOST', '<Enter here>');
+define('DB_USER', '<Enter here>');
+define('DB_PASSWORD', '<Enter here>');
+define('DB_NAME', '<Enter here>');
